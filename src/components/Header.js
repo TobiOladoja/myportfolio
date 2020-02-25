d7d5f082e17d0d3595763ee452a7e784e972403b
TobiOladoja <tbfullstack@gmail.com>
2020-02-25 12:06:32 +0000

    75% of Header, added padding arround it, with logo and text inside

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Logo from '../img/logo-white.png';
 
 const Header = () => {
   return (
-    <div>
-      <header className='header'>Some text...</header>
-    </div>
+    <header className='header'>
+      <div className='logo-box'>
+        <img src={Logo} alt='Logo' className='logo' />
+      </div>
+      <div className='text-box'>
+        <h1 className='heading-primary'>
+          <span className='heading-primary-main'>Tobi Oladoja</span>
+          <span className='heading-primary-sub'>Full-Stack Developer</span>
+        </h1>
+      </div>
+    </header>
   );
 };
 
