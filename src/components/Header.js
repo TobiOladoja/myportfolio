5806e34c2b1158a07eb87e4e8890f93b63de950c
TobiOladoja <tbfullstack@gmail.com>
2020-02-28 17:47:27 +0000

    Converted architecture to BEM

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,16 @@ import Logo from '../img/logo-white.png';
 const Header = () => {
   return (
     <header className='header'>
-      <div className='logo-box'>
-        <img src={Logo} alt='Logo' className='logo' />
+      <div className='header__logo-box'>
+        <img src={Logo} alt='Logo' className='header__logo' />
       </div>
-      <div className='text-box'>
+      <div className='header__text-box'>
         <h1 className='heading-primary'>
-          <span className='heading-primary-main'>Tobi Oladoja</span>
-          <span className='heading-primary-sub'>Full-Stack Developer</span>
+          <span className='heading-primary--main'>Tobi Oladoja</span>
+          <span className='heading-primary--sub'>Full-Stack Developer</span>
         </h1>
 
-        <a href='#' className='btn btn-white btn-animated'>
+        <a href='#' className='btn btn--white btn--animated'>
           See Projects
         </a>
       </div>
